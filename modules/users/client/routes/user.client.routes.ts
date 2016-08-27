92bcabb214fb1833fb7af9006fb4b5776c1c42db
Don Ebben <don.ebben@gmail.com>
2016-08-27 11:13:06 -0400

    Added auth gaurds to the admin routes.

--- modules/users/client/routes/user.client.routes.ts
+++ modules/users/client/routes/user.client.routes.ts
@@ -8,17 +8,48 @@ import { EditUser } from '../components/admin/editUser.component';
 export const AdminRoutes: RouterConfig = [
   {
     path: 'admin',
+    data: {
+      menu: {
+        title: 'Administration',
+        icon: 'ion-android-person',
+        selected: false,
+        expanded: false,
+        order: 9999
+      }
+    },
     children: [{
       path: 'users',
-      component: ListUsers
+      component: ListUsers,
+      data: {
+        menu: {
+          title: 'Manage Users',
+          icon: 'ion-android-person',
+          selected: false,
+          expanded: false,
+          order: 1
+        },
+        roles: [
+          'admin'
+        ]
+      }
   },
   {
     path: 'users/:userId',
-    component: User
+    component: User,
+    data: {
+      roles: [
+        'admin'
+      ]
+    }
   },
   {
     path: 'users/:userId/edit',
-    component: EditUser
+    component: EditUser,
+    data: {
+      roles: [
+        'admin'
+      ]
+    }
   }]
   }
 ];
